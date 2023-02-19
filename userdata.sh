ea11b241c6eb3385c444133e44c47a63e1ac47e3
Xavier T <xavier.torres.alonso@gmail.com>
2023-02-19 22:00:38 +0100

    remove ssm

--- userdata.sh
+++ userdata.sh
@@ -6,11 +6,10 @@ app_user="appuser"
 install_script="install.sh"
 ##END OF PARAMETERS##
 yum update -y
-yum install -y git 
+yum install -y git
 useradd -m "$app_user"
 mkdir -p $app_dir
 chown -R "${app_user}:${app_user}" "$app_basedir"
 su - "$app_user" -c "git clone https://github.com/dext0s/django_app.git $app_dir"
 su - "$app_user" -c "cd $app_dir; chmod +x $install_script; ./${install_script}"
 #MISSING CLOUDFORMATION COMMANDS
-#
